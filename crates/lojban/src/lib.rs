d12ba6a45d5fe9e37e6bc08cc400c020fcfc9fa1
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-12-01 20:34:54 -0800

    add

--- crates/lojban/src/lib.rs
+++ crates/lojban/src/lib.rs
@@ -1,6 +1,10 @@
 pub mod vlalehu {
 	//! Representations of Lojban characters with type-level niceties
 
+	use stodi::Stodi;
+
+	/// TODO is there an official lujvo for this already?
+	///
 	/// Lojban character literal stored in memory as a UTF-8 compatible code point using [char]
 	pub struct Lerfu(char);
 
@@ -9,5 +13,22 @@ pub mod vlalehu {
 			'\'', ',', '.', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
 			'r', 's', 't', 'u', 'v', 'x', 'y', 'z',
 		];
+	}
+
+	impl stodi::Stodi for Lerfu {
+		fn check_stodi(&self) -> bool {
+			Self::ALLOWED.contains(&self.0)
+		}
+	}
+
+	impl Lerfu {
+		pub unsafe fn new_unchecked(c: char) -> Self {
+			Lerfu(c)
+		}
+
+		pub fn new(c: char) -> Option<Self> {
+			let ret = unsafe { Lerfu::new_unchecked(c) };
+			ret.check_stodi().then_some(ret)
+		}
 	}
 }
--- crates/stodi/src/lib.rs
+++ crates/stodi/src/lib.rs
@@ -1,6 +1,6 @@
 /// For invariants that are cheap to check
 pub trait Stodi {
-	fn check_invariant(&self) -> bool;
+	fn check_stodi(&self) -> bool;
 }
 
 /// TODO make a new stodi-macros crate which adds this attribute
