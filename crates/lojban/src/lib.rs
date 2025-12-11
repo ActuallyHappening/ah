0606cf383d31f7268cf9479beb32a027f55870ad
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-12-11 14:40:59 -0800

    add: ah-tcita

--- crates/lojban/src/lib.rs
+++ crates/lojban/src/lib.rs
@@ -78,29 +78,29 @@ pub mod le_lerfu {
 	/// Lojban character
 	/// ```
 	#[derive(Clone, Copy)]
-	pub struct Jbolehu(u8);
+	pub struct Le_jbolehu(u8);
 
-	impl Stodi for Jbolehu {
+	impl Stodi for Le_jbolehu {
 		fn check_stodi(&self) -> bool {
 			Sohi_lojbo_selylehu::default().contains(&self.0)
 				&& format!("{}", self) == self.char().to_string()
 		}
 	}
 
-	impl Jbolehu {
+	impl Le_jbolehu {
 		#[contracts::requires(self.check_stodi())]
 		pub fn char(self) -> char {
 			char::try_from(self.0).unwrap()
 		}
 	}
 
-	impl std::fmt::Display for Jbolehu {
+	impl std::fmt::Display for Le_jbolehu {
 		fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 			write!(f, "{}", self.char())
 		}
 	}
 
-	impl std::fmt::Debug for Jbolehu {
+	impl std::fmt::Debug for Le_jbolehu {
 		fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 			f.debug_tuple("Jbole'u").field(&self.0).finish()
 		}
---
+++ crates/tcita/src/lib.rs
@@ -0,0 +1,25 @@
+//! ```lojban
+//! si'o tcita gau la lojban
+//! ```
+//! ```glico
+//! Label in language lojban
+//! ```
+
+/// ```lojban
+/// ka tcita
+/// ```
+/// ```glico
+/// The property of being|having a label
+/// ```
+pub trait Ka_tcita {
+	/// ```lojban
+	/// ti seltcita bau la lojban
+	/// .idu ti se tcita bau la lojban
+	/// ```
+	/// ```glico
+	/// This thing's label in Lojban
+	/// ```
+	const TI_SELTCITA_BAU_LA_LOJBAN: &str;
+	/// [Self::TI_SELTCITA_BAU_LA_LOJBAN]
+	const TCITA: &str = Self::TI_SELTCITA_BAU_LA_LOJBAN;
+}
