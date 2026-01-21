eb5ea07ee8dfad778f275986a06d12445d1b1c84
Caleb Yates <calebyates42@proton.me>
2026-01-21 02:26:25 +0000

    todo

--- crates/timetracker/src/timetracker/span.rs
+++ crates/timetracker/src/timetracker/span.rs
@@ -2,20 +2,20 @@ use ah_persistence::{PersistenceEngine, SidboBuilder, packaji_sidbo, sidbo::Sidb
 
 use crate::{prelude::*, timetracker::Timetracker};
 
-packaji_sidbo!(pub struct SpanFasnu {
+packaji_sidbo!(pub struct SpanFasnuSidbo {
 	ckaji: FasnuCkaji,
 	...
 });
 
-impl Ka_tcita for SpanFasnu {
+impl Ka_tcita for SpanFasnuSidbo {
 	const TI_SELTCITA_BAU_LA_LOJBAN: &str = "TODO ah-timetracker fasnu span sidbo";
 }
 
-impl SpanFasnu {
+impl SpanFasnuSidbo {
 	fn derive_name() -> String {
 		format!(
 			"{} noi randomness {}",
-			SpanFasnu::TCITA,
+			SpanFasnuSidbo::TCITA,
 			ah_lojban::quote(uuid::Uuid::new_v4().to_string()),
 		)
 	}
@@ -62,9 +62,9 @@ pub struct StopBuilder {
 
 impl Timetracker {
 	#[instrument(skip_all)]
-	pub async fn start(&self, fasnu: StartBuilder) -> Result<SpanFasnu> {
+	pub async fn start(&self, fasnu: StartBuilder) -> Result<SpanFasnuSidbo> {
 		let sidbo =
-			SidboBuilder::new(SpanFasnu::derive_name()).add_ckaji(FasnuCkaji::Start(Start {
+			SidboBuilder::new(SpanFasnuSidbo::derive_name()).add_ckaji(FasnuCkaji::Start(Start {
 				start: UtcDateTime::now().into(),
 				project: fasnu.project,
 				billing_company: fasnu.billing_company,
@@ -74,25 +74,118 @@ impl Timetracker {
 			.add(sidbo)
 			.await
 			.wrap_err("Couldn't start")?;
-		let ret = SpanFasnu::try_from(sidbo)?;
+		let ret = SpanFasnuSidbo::try_from(sidbo)?;
 		info!("Started a span");
 		Ok(ret)
 	}
 
 	#[instrument(skip_all)]
-	pub async fn stop(&self, fasnu: StopBuilder) -> Result<SpanFasnu> {
-		let sidbo = SidboBuilder::new(SpanFasnu::derive_name()).add_ckaji(FasnuCkaji::Stop(Stop {
-			stop: fasnu.stop.into(),
-			project: fasnu.project,
-			billing_company: fasnu.billing_company,
-		}))?;
+	pub async fn stop(&self, fasnu: StopBuilder) -> Result<SpanFasnuSidbo> {
+		let sidbo =
+			SidboBuilder::new(SpanFasnuSidbo::derive_name()).add_ckaji(FasnuCkaji::Stop(Stop {
+				stop: fasnu.stop.into(),
+				project: fasnu.project,
+				billing_company: fasnu.billing_company,
+			}))?;
 		let sidbo = self
 			.persistence
 			.add(sidbo)
 			.await
 			.wrap_err("Couldn't stop")?;
-		let ret = SpanFasnu::try_from(sidbo)?;
+		let ret = SpanFasnuSidbo::try_from(sidbo)?;
 		info!("Stopped a span");
 		Ok(ret)
+	}
+}
+
+pub mod processing {
+	use std::collections::HashMap;
+
+	use ah_persistence::sidbo::SidboTcita;
+
+	use crate::{
+		prelude::*,
+		timetracker::{
+			Timetracker,
+			span::{FasnuCkaji, SpanFasnuSidbo, Start, Stop, StopBuilder},
+		},
+	};
+
+	impl Timetracker {
+		async fn get_spans_raw(&self) -> Result<Vec<SpanFasnuSidbo>> {
+			let ids = self.persistence.select_ckaji_sidbo::<FasnuCkaji>().await?;
+			let spans = self.persistence.select_sidbo(ids).await?;
+			Ok(spans)
+		}
+
+		pub async fn get_spans(&self) -> Result<UnderstandSpanState> {
+			let full = self.get_spans_raw().await?;
+			Ok(UnderstandSpanState { full })
+		}
+	}
+
+	pub struct UnderstandSpanState {
+		full: Vec<SpanFasnuSidbo>,
+	}
+
+	#[derive(Default)]
+	pub struct ResolvedSpanState {
+		inner: HashMap<SidboTcita, HashMap<SidboTcita, SpansState>>,
+	}
+
+	#[derive(Default)]
+	pub struct SpansState {
+		history: Vec<ClosedSpan>,
+		open: Option<OpenSpan>,
+	}
+
+	pub struct ClosedSpan {
+		pub(crate) start: Start,
+		pub(crate) start_id: SidboTcita,
+		pub(crate) stop: Stop,
+		pub(crate) stop_id: SidboTcita,
+	}
+
+	pub struct OpenSpan {
+		pub(crate) start: Start,
+		pub(crate) start_id: SidboTcita,
+	}
+
+	impl UnderstandSpanState {
+		pub fn resolve(self) -> Result<ResolvedSpanState> {
+			let mut ret = ResolvedSpanState::default();
+			for span in self.full {
+				match span.ckaji() {
+					FasnuCkaji::Start(start) => {
+						ret
+							.get(&start.billing_company, &start.project)
+							.start(start)?;
+					}
+					FasnuCkaji::Stop(stop) => {}
+				}
+			}
+			Ok(ret)
+		}
+	}
+
+	impl SpansState {
+		fn start(&mut self, start: &Start) -> Result<()> {
+			todo!()
+		}
+
+		fn stop(&mut self, stop: &Stop) -> Result<()> {
+			todo!()
+		}
+	}
+
+	impl ResolvedSpanState {
+		fn get(&mut self, company: &SidboTcita, project: &SidboTcita) -> &mut SpansState {
+			self
+				.inner
+				.entry(company.clone())
+				.or_default()
+				.entry(project.clone())
+				.or_default()
+		}
 	}
 }
--- crates/timetracker/src/timetracker_ckaji.rs
+++ crates/timetracker/src/timetracker_ckaji.rs
@@ -19,18 +19,6 @@ struct Active {
 }
 
 #[veciksi(
-	lojban = "TODO ah-timetracker su'u tcika span",
-	glico = "Span (in time) abstraction"
-)]
-#[tcita("TODO ah-timetracker su'u tcika span")]
-#[derive(Debug, Serialize, Deserialize)]
-pub struct SpanCkaji {
-	pub(crate) id: SidboTcita,
-	pub(crate) start: UtcDateTime,
-	pub(crate) duration: Duration,
-}
-
-#[veciksi(
 	lojban = "TODO ah-timetracker su'u billing company",
 	glico = "Billing Company abstraction"
 )]
