b6336f2d61b498cd3cef04ce9738b856fb42292b
Caleb Yates <calebyates42@proton.me>
2026-01-23 16:46:47 +1000

    feat(Timetracker): data loads!

--- sazycimde/iced/src/prelude.rs
+++ sazycimde/iced/src/prelude.rs
@@ -1,4 +1,11 @@
+#![allow(unused_imports)]
+
 pub(crate) use crate::*;
 
 pub(crate) use derive_more::From;
-pub(crate) use iced::{Element, Theme, widget::button};
+pub(crate) use tracing::{debug, error, info, trace, warn};
+
+pub(crate) use iced::{
+	Element, Theme,
+	widget::{self, button, column, row, text},
+};
--- sazycimde/iced/src/timetracker.rs
+++ sazycimde/iced/src/timetracker.rs
@@ -15,30 +15,37 @@ use crate::{prelude::*, toplevel::TopLevelMessage};
 #[derive(Default, Clone, Debug)]
 pub struct State {
 	by_project: Option<ProjectResolved<SpansByDay>>,
+	err: Option<String>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
 	Loaded(ProjectResolved<SpansByDay>),
+	Err(String),
 }
 
 impl State {
 	pub(crate) fn start() -> Task<Message> {
-		Task::done(Message::Loaded(Default::default()))
+		Task::perform(fetch_spans(), |data| match data {
+			Ok(data) => Message::Loaded(data),
+			Err(err) => Message::Err(err.to_string()),
+		})
 	}
 
 	pub(crate) fn view(&self) -> Element<'static, TopLevelMessage> {
-		text!("{:?}", self.by_project).into()
+		widget::column![text!("{:?}", self.by_project), text!("{:?}", self.err)].into()
 	}
 
 	pub(crate) fn update(&mut self, message: Message) {
 		match message {
 			Message::Loaded(data) => self.by_project = Some(data),
+			Message::Err(err) => self.err = Some(err),
 		}
 	}
 }
 
 pub async fn fetch_spans() -> color_eyre::Result<ProjectResolved<SpansByDay>> {
+	info!("Fetching data");
 	let timetracker = Timetracker::new().await?;
 	let spans = timetracker.get_spans().await?.resolve()?;
 	// uinai hard coded +10
