da5f2a402f7a67076a7cb076e6716c1c54388ef9
Caleb Yates <calebyates42@proton.me>
2026-01-23 14:52:18 +1000

    add(ABI-3192): zo'e
    This commit message is auto-generated
    Learn Lojban: https://lojban.pw/en/books/learn-lojban/!1/

--- sazycimde/iced/src/lib.rs
+++ sazycimde/iced/src/lib.rs
@@ -30,7 +30,7 @@ pub mod toplevel {
 	impl TopLevelState {
 		pub fn update(&mut self, message: TopLevelMessage) {
 			match message {
-				TopLevelMessage::Home(change) => match change {
+				TopLevelMessage::ChangeScreen(change) => match change {
 					homescreen::ChangeScreens::Timetracker => self.screen = ScreensState::Timetracker,
 				},
 			}
@@ -38,8 +38,8 @@ pub mod toplevel {
 
 		pub fn view(&self) -> Element<'_, TopLevelMessage> {
 			match &self.screen {
-				ScreensState::Home => homescreen::home().map(TopLevelMessage::Home),
-				ScreensState::Timetracker => timetracker::timetracker(),
+				ScreensState::Home => homescreen::home().map(TopLevelMessage::ChangeScreen),
+				ScreensState::Timetracker => timetracker::view(),
 			}
 		}
 
@@ -50,30 +50,57 @@ pub mod toplevel {
 
 	#[derive(Debug, Clone)]
 	pub enum TopLevelMessage {
-		Home(homescreen::ChangeScreens)
+		ChangeScreen(homescreen::ChangeScreens),
 	}
 }
 
 pub mod homescreen {
-	pub use crate::prelude::*;
+	use iced::Length::Fill;
+
+	use crate::prelude::*;
 	use crate::toplevel::{ScreensState, TopLevelMessage};
-	
+
 	#[derive(Debug, Clone)]
 	pub(crate) enum ChangeScreens {
 		Timetracker,
 	}
 
 	pub(crate) fn home() -> Element<'static, ChangeScreens> {
-		button("Timetracker").on_press(ChangeScreens::Timetracker).into()
+		button("Timetracker")
+			.width(Fill)
+			.height(Fill)
+			.on_press(ChangeScreens::Timetracker)
+			.into()
 	}
 }
 
 pub mod timetracker {
-	use crate::{prelude::*, toplevel::TopLevelMessage};
-	
-	// pub(crate) 
+	use std::collections::HashMap;
 
-	pub(crate) fn timetracker() -> Element<'static, TopLevelMessage> {
-		button("this is your timetracker").into()
+	use ah_timetracker::timetracker::span::processing::{ProjectResolved, ProjectResolvedSpanState, SpansState};
+	use iced::widget::Row;
+	use time::Date;
+
+	use crate::{prelude::*, toplevel::TopLevelMessage};
+
+	pub(crate) struct State {
+		by_project: ProjectResolved<SpansByDay>
+	}
+
+	pub struct SpansByDay {
+		clean: HashMap<Date, SpansState>,
+		unclean: SpansState,
+	}
+	
+	impl State {
+		fn split_by_day(project_resolved: &ProjectResolvedSpanState) -> ProjectResolved<SpansByDay> {
+				let mut row = Row::with_capacity(times.len());
+				todo!()
+		}
+	}
+
+	pub(crate) fn view(state: &State) -> Element<'static, TopLevelMessage> {
+		let times = vec![1, 2, 3];
+		todo!()
 	}
 }
